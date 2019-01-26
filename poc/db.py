5a652661ecb5e6b3ccacfb37f9b2fa4162a7268b
Michel <michel@mail.com>
2019-01-26 18:28:30 +0100

    Device

--- poc/db.py
+++ poc/db.py
@@ -4,7 +4,8 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.exc import IntegrityError
 
-from db_schema import Device, Mapping, DevMem, Base
+from db_schema import Device, Mapping, Base
+import re
 
 import logging
 logging.basicConfig(filename='db.log', level=logging.DEBUG)
@@ -18,19 +19,43 @@ def create_session():
     DBSession = sessionmaker(bind=engine)
     return DBSession()
 
-def e_device(session, _name, _bdf):
-    pass
+def e_device(session, _name, _bdf, _lspci):
+    m = re.findall('(?<=Memory at )(?:\w+)(?= \()', _lspci)
+    existing_d = session.query(Device).filter_by(bdf=_bdf).one_or_none()
+
+    if existing_d:
+        logging.warning("Device {} already exist, not adding"
+                .format(new_device.bdf))
+        return existing_d
+    else:
+        new_device = Device(
+                name = _name,
+                bdf = _bdf,
+                mapping = Mapping(
+                    phys_addr = m[0] if m else "0x00000000",
+                    iova = "0x0000000"
+                )
+        )
+        session.add(new_device)
+        session.commit()
+        logging.info('Added device {} with base PA {}'
+                .format(
+                    new_device.bdf, 
+                    new_device.mapping.phys_addr
+                    ))
+        return new_device
+
 
 def e_map(session, _iova, _phys_addr, _size):
     existing_p = session.query(Mapping).filter_by(phys_addr=_phys_addr).one_or_none()
     existing_v = session.query(Mapping).filter_by(iova=_iova).one_or_none()
 
+    new_mapping = Mapping(
+            iova = _iova,
+            phys_addr = _phys_addr,
+            size = _size,
+            )
     if not existing_v and not existing_p:
-        new_mapping = Mapping(
-                iova = _iova,
-                phys_addr = _phys_addr,
-                size = _size,
-                )
         session.add(new_mapping)
         session.commit()
         logging.info('Mapped v:{} p:{}'
@@ -39,27 +64,45 @@ def e_map(session, _iova, _phys_addr, _size):
                     new_mapping.phys_addr 
                     ))
         return new_mapping
-    elif existing_v:
-        raise MapException('Virtual address {} is already mapped to physical address {}'
+    elif existing_v and existing_v.phys_addr != new_mapping.phys_addr:
+        logging.info('(Update) PA <{}> => <{}> for VA {}'
                 .format(
-                    existing_v.iova,
                     existing_v.phys_addr,
+                    new_mapping.phys_addr,
+                    existing_v.iova
                 ))
-    elif existing_p:
-        raise MapException('Physical address {} is already mapped to virtual address {}'
+        existing_v.phys_addr = new_mapping.phys_addr
+        session.commit()
+        return existing_v
+
+    elif existing_p and existing_p.iova != new_mapping.iova:
+        logging.info('(Update) VA <{}> => <{}> for PA {}'
                 .format(
-                    existing_p.phys_addr,
-                    existing_p.iova
+                    existing_p.iova,
+                    new_mapping.iova,
+                    existing_p.phys_addr
                     ))
+        existing_p.iova = new_mapping.iova
+        session.commit()
+        return existing_p
 
 def handle_event(session, event):
-    mapped = e_map(
-            session,
-            event['iova'],
-            event['phys_addr'],
-            event['size']
-    )
-    return mapped
+    if event['type'] == 'map':
+        mapped = e_map(
+                session,
+                event['iova'],
+                event['phys_addr'],
+                event['size']
+        )
+        return mapped
+    elif event['type'] == 'device':
+        e_device(
+                session,
+                event['name'],
+                event['bdf'],
+                event['lspci']
+                )
+
 
 def create_db_from_parse():
     session = create_session()
@@ -88,35 +131,54 @@ def main():
 
 def _retrieve_events():
     return [
+            {
+                '_debug': 'a device with a mapping',
+                'type': 'device',
+                'name': 'GTX 960M',
+                'bdf': '01:00.00',
+                'lspci': "Memory at ee000000 (32-bit, non-prefetchable) [size=16M]"
+            },
+            { 
+                '_debug': 'The mapping corresponding to previous attached dev',
+                'type': 'map',
+                'iova': '0x000faf',
+                'size': 16384,
+                'phys_addr': 'ee000000'
+            },
             { 
                 '_debug': 'a working map',
+                'type': 'map',
+                'iova': '0x000400',
+                'size': 2048,
+                'phys_addr': 'aa000fa'
+            },
+            { 
+                '_debug': 'a working map',
+                'type': 'map',
                 'iova': '0x000600',
                 'size': 2048,
-                'phys_addr': '0xaa000fa'
+                'phys_addr': 'bb000fa'
             },
             { 
                 '_debug': 'a working map',
+                'type': 'map',
+                'iova': '0x000800',
+                'size': 2048,
+                'phys_addr': 'cc000fa'
+            },
+            { 
+                '_debug': 'same iova than the working map, should update',
+                'type': 'map',
                 'iova': '0x000400',
                 'size': 2048,
-                'phys_addr': '0xff000fa'
+                'phys_addr': 'aa000fb'
             },
             { 
-                '_debug': 'a working map',
-                'iova': '0x000408',
+                '_debug': 'same phys_addr than working map, should update',
+                'type': 'map',
+                'iova': '0x000a00',
                 'size': 2048,
-                'phys_addr': '0xff000ff'
-            },
-            { 
-                '_debug': 'same iova than the working map, should fail',
-                'iova': '0x000400',
-                'size': 2048,
-                'phys_addr': '0xff000fb'
-            },
-            { 
-                '_debug': 'same phys_addr than working map, should fail',
-                'iova': '0x000404',
-                'size': 2048,
-                'phys_addr': '0xff000fa'
+                'phys_addr': 'bb000fa'
             },
     ]
 
--- poc/db_schema.py
+++ poc/db_schema.py
@@ -14,21 +14,14 @@ class Device(Base):
     id            = Column(Integer, primary_key=True)
     name          = Column(String(255), unique=True)
     bdf           = Column(String(255), unique=True)
-
-class DevMem(Base):
-    __tablename__ = 'devmem'
-    id            = Column(Integer, primary_key=True)
-    device_id     = Column(Integer, ForeignKey('device.id'))
-    device        = relationship('Device', backref='devmem')
     mapping_id    = Column(Integer, ForeignKey('mapping.id'))
-    mapping       = relationship('Mapping', backref='devmem')
-
+    mapping       = relationship('Mapping', backref='device')
 
 class Mapping(Base):
     __tablename__ = 'mapping'
     id            = Column(Integer, primary_key=True)
-    iova          = Column(String(255), unique=True)
-    phys_addr     = Column(String(255), unique=True)
+    iova          = Column(String(255))
+    phys_addr     = Column(String(255))
     size          = Column(Integer)
 
 engine = create_engine('sqlite:///iommu.db')
