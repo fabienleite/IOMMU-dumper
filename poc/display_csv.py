47ca91ee9a09d2655a73ff94609a0a1cc36e5b37
Melanie Romain <melanie.romain35@gmail.com>
2019-01-27 14:08:52 +0100

    Updating csv display

--- poc/display_csv.py
+++ poc/display_csv.py
@@ -1,7 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from db_schema import Device, Mapping, Base
-import csv
+import csv, os
 
 def create_session():
     """ Create a session to retrieved data from iommu.db.
@@ -20,21 +22,26 @@ def main():
 
     session = create_session()
 
+    if not os.path.isdir('out'):
+        os.makedirs('out')
+
     with open('out/iommu_config_display.csv', 'w') as output_csv:
         output_writer = csv.writer(output_csv, delimiter=';')
 
         # ---- Header
-        output_writer.writerow(['IOVA','Physical adress', 'Size','Device name'])
+        output_writer.writerow(['MAPPING','IOVA','PHYSICAL ADDRESS', 'SIZE','DEVICE B.D:F', 'DEVICE NAME'])
 
         for map in session.query(Mapping).all():
-            device = session.query(Device).filter_by(memoryBaseAddress=map).one_or_none()
+            device = session.query(Device).filter_by(mapping=map).one_or_none()
 
             if device is not None:
                 device_name = device.name
+                device_bdf = device.bdf
             else:
                 device_name = ""
+                device_bdf = ""
 
-            output_writer.writerow([map.iova, map.phys_addr, map.size, device_name])
+            output_writer.writerow([map.id, map.iova, map.phys_addr, map.size, device_bdf, device_name])
 
 
 if __name__ == '__main__':
