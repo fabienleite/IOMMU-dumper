d91c2c9a599ede7bf21e600c18cefdc3592a0d35
Fabien Leite <fabienleite@protonmail.com>
2019-01-27 18:40:59 +0100

    HTML display with everything working fine if database is valid

--- poc/display_html.py
+++ poc/display_html.py
@@ -9,6 +9,7 @@ import shutil
 from db_schema import Device
 from hole_calculator import calc_all_holes, get_all_mappings
 from db import create_session
+from math import floor
 
 
 def generate_html_frieze(type, value):
@@ -33,17 +34,17 @@ def generate_html_frieze(type, value):
     for hole in holes_raw:
         holes.append(
             {
-                'devices_id': -1000,
-                'id': -1000,
-                'iova': None,
-                'phys_addr': hole[0],
-                'size': hole[1]
+                "devices_id": -1000,
+                "id": -1000,
+                "iova": None,
+                "phys_addr": hole[0],
+                "size": hole[1],
             }
         )
 
     for hole in holes:
-        hole['devices_id'] = -1
-    
+        hole["devices_id"] = -1
+
     try:
         mappings = add_device_info(mappings, session)
     except:
@@ -53,9 +54,11 @@ def generate_html_frieze(type, value):
     mappings_as_dict = []
     for m in mappings:
         mappings_as_dict.append(m.__dict__)
-    memory_state = sorted(mappings_as_dict + holes, key=lambda mapping: mapping['phys_addr'])
+    memory_state = sorted(
+        mappings_as_dict + holes, key=lambda mapping: mapping["phys_addr"]
+    )
     memory_state = unify_common_space(memory_state)
-    html_frieze = create_html_from_memory_state(memory_state)
+    html_frieze = create_html_from_memory_state(memory_state, session)
     return html_frieze
 
 
@@ -70,8 +73,11 @@ def unify_common_space(memory_state):
     """
     new_memory_state = [memory_state[0]]
     for i in range(1, len(memory_state)):
-        if memory_state[i]['devices_id'] == 0 and new_memory_state[-1]['devices_id'] == 0:
-            new_memory_state[-1]['size'] += memory_state[i]['size']
+        if (
+            memory_state[i]["devices_id"] == 0
+            and new_memory_state[-1]["devices_id"] == 0
+        ):
+            new_memory_state[-1]["size"] += memory_state[i]["size"]
         else:
             new_memory_state.append(memory_state[i])
     return new_memory_state
@@ -90,13 +96,11 @@ def add_device_info(mappings, session):
     """
     for mapping in mappings:
         mapping_device = (
-            session.query(Device)
-            .filter(Device.mapping == mapping)
-            .one_or_none()
+            session.query(Device).filter(Device.mapping == mapping).one_or_none()
         )
         if mapping_device != None:
             # Devices map exactly one segment for them specifically
-            mapping.devices_id = Device.id
+            mapping.devices_id = mapping_device.id
         else:
             # If the mapping is in the mapping table and not related to a Device,
             # it's part of the common space to be used
@@ -104,47 +108,74 @@ def add_device_info(mappings, session):
     return mappings
 
 
-def create_html_from_memory_state(memory_state):
+def create_html_from_memory_state(memory_state, session):
     """
     Generates the "frieze" for the html output
     The frieze is an html table
 
     Input:
         - memory_state : the list of memory states : holes and device mappings
+        - session : the sqlalchemy session
     """
     i = 0
     frieze_text = ""
+    total_size = 0
+    for memory_part in memory_state: total_size += memory_part['size'] 
     for memory_part in memory_state:
-        hole = ' hole' if memory_part['devices_id'] == -1 else ''
-        domain_shared = ' domain-shared' if memory_part['devices_id'] == 0 else ''
-        if hole != ' hole' and domain_shared != ' domain-shared' :
+        hole = " hole" if memory_part["devices_id"] == -1 else ""
+        domain_shared = " domain-shared" if memory_part["devices_id"] == 0 else ""
+
+        if hole != " hole" and domain_shared != " domain-shared":
             color_id = (i % 3) + 1
-            dev_name = 'Temporary Name'
-        elif hole != ' hole':
-            dev_name = 'Domain shared space'
-            color_id = ''
-        elif domain_shared != ' domain-shared':
-            dev_name = 'Hole'
-            color_id = ''
+            mapping_device = (
+                session.query(Device)
+                .filter(Device.id == memory_part["devices_id"])
+                .one_or_none()
+            )  # not a clean way of doing it !
+            dev_name = mapping_device.name
+        elif hole != " hole":
+            dev_name = "Domain shared space"
+            color_id = ""
+        elif domain_shared != " domain-share":
+            dev_name = "Hole"
+            color_id = ""
+
+        repr_size = 0
+        if float(memory_part['size'] / total_size) > 0.8:
+            repr_size = 8
+        elif float(memory_part['size'] / total_size) < 0.1 and dev_name == "Domain shared space" :
+            repr_size = 2
+        elif float(memory_part['size'] / total_size) < 0.1:
+            repr_size = 1
+        else :
+            repr_size = floor(float(memory_part['size'] / total_size) * 100)
 
         frieze_text += (
             "<td "
             + 'class="memory-range color'
             + str(color_id)
-            + hole + domain_shared + '"'
+            + hole
+            + domain_shared
+            + '"'
             + 'data-device-id="'
-            + str(memory_part['devices_id'])
+            + str(memory_part["devices_id"])
+            + '"'
+            + 'data-size="'
+            + str(repr_size)
             + '"'
             + "onmouseover=\"displayDeviceInformation('"
-            + str(memory_part['devices_id'])
-            + "', '" + dev_name + "', '"
-            + str(memory_part['iova'])
+            + str(memory_part["devices_id"])
+            + "', '"
+            + dev_name
+            + "', '"
+            + str(memory_part["iova"])
             + "','"
-            + str(memory_part['phys_addr'])
+            + str(memory_part["phys_addr"])
             + "->"
-            #+ str(hex(int(memory_part['phys_addr'], 16) + memory_part['size']))
-            + "')\", onmouseout=\"revertToNormalOpacityAndText();\""
-            +  ">" + "</td>"
+            + str(hex(int(memory_part['phys_addr'], 16) + memory_part['size']))
+            + '\')", onmouseout="revertToNormalOpacityAndText();"'
+            + ">"
+            + "</td>"
         )
         i += 1
 
@@ -192,6 +223,7 @@ def write_html_file(type=None, value=None):
     shutil.copy2("./html/normalize.css", "./out/")
 
     open_file("./out/iommu_config_display.html")
+    print("Everything worked fine, go check your generated HTML in your browser !")
 
 
 def open_file(path):
